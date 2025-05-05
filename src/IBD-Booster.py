c8037c0ce91684192dfa1115a1870094b23e89c1
Alex Mercadel <alexmercadel@Alexs-MacBook-Pro.local>
2025-05-05 13:24:17 -0500

    fixed some issues with pandas dataframes

--- src/IBD-Booster.py
+++ src/IBD-Booster.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import subprocess
-from typing import Tuple
+import time
 
 
 import torch
@@ -11,11 +11,10 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 
-from ibdInference import hap_ibd, p_smoother, checkRateMapType
-from convertHapMapToPLINK import convert as convert_hap_map_to_plink
-from constructDataset import constructDataset
+from ibdInference import hap_ibd, p_smoother
+from constructDataset import constructBoostedDataset
+from formatSegments import formatSegments
 
-    
 
 
 class Net(nn.Module):
@@ -42,41 +41,6 @@ class Net(nn.Module):
         x = self.activation_output(self.output(x))
         return x
 
-
-def augmentSegments(df: pd.DataFrame):
-    """
-    this function loads in the the neural network and XGBoost models, then filters them according to the
-    predictions made by each model, returning the original dataframe with 2 new columns (1 for each prediction)
-    """
-    X, _ = process_df(df)
-    model = Net()
-    model.load_state_dict(torch.load("../models/nn_model_state_dict.model", weights_only=True))
-
-    xgb_model = xgb.XGBClassifier()
-    xgb_model.load_model("../models/xgb_ibd_augmentation.json")
-    y_pred_xgb = pd.Series(xgb_model.predict(X))
-
-    X_tens = torch.tensor(X.values, dtype = torch.float32)
-    y_pred_nn = pd.Series((model(X_tens) > 0.5).int())
-    df['classification'] = df['classification'] - 2
-    df = df.reset_index().drop('index', axis = "columns")
-    df['xgb_pred'] = y_pred_xgb
-    df['nn_pred'] = y_pred_nn
-    return df
-
-
-def process_df(df: pd.DataFrame) -> Tuple[pd.Dataframe, pd.DataFrame]: 
-    """
-    simple processing of the input file: removal of non-feature columns and ground truth segments (if included)
-    """
-    exclude = ["id1", "hap1", "id2", "hap2", "start", "end", "classification", "hap_idx1", "hap_idx2"]
-    df['classification'] = df['classification'] - 2
-    X = df[[i for i in df.columns if i not in exclude]]
-    y = df['classification']
-    scaler = StandardScaler()
-    X = pd.DataFrame(scaler.fit_transform(X), columns = X.columns)
-    return X, y
-
 def df_to_file(df: pd.DateOffset, output_file: str, col: str) -> None:
     """
     writes df to a file for downstream use
@@ -90,9 +54,60 @@ def df_to_file(df: pd.DateOffset, output_file: str, col: str) -> None:
             f.write(f"{row['id1']}\t{row['hap1'] + 1}\t{row['id2']}\t{row['hap2'] + 1}\t20\t{row['start']}\t{row['end']}\t{cm_len}\n")
     f.close()
 
+def augmentSegments(df: pd.DataFrame):
+    """
+    this function loads in the the neural network and XGBoost models, then filters them according to the
+    predictions made by each model, returning the original dataframe with 2 new columns (1 for each prediction)
+    """
+    exclude = ["id1", "hap1", "id2", "hap2", "start", "end", "classification", "hap_idx1", "hap_idx2"]
+    X = df[[i for i in df.columns if i not in exclude]]
+    scaler = StandardScaler()
+    X = pd.DataFrame(scaler.fit_transform(X), columns = X.columns)
+    model = Net()
+    model.load_state_dict(torch.load("models/nn_model_state_dict.model", weights_only=True))
+
+    xgb_model = xgb.XGBClassifier()
+    xgb_model.load_model("models/xgb_ibd_augmentation.json")
+    y_pred_xgb = pd.Series(xgb_model.predict(X))
+
+    X_tens = torch.tensor(X.values, dtype = torch.float32)
+    y_pred_nn = pd.Series((model(X_tens) > 0.5).int().flatten())
+
+    df = df.reset_index().drop('index', axis = "columns")
+    df['xgb_pred'] = y_pred_xgb
+    df['nn_pred'] = y_pred_nn
+    return df
+
+
+
 
    
+def formatSegmentsBoosted(hap_ibd_res_fp: str, output_hap: str) -> None:
+	"""This function condenses the ground truth segments and the hap-IBD reported segments to the same format
 
+	Args:
+		hap_ibd_res_fp (str): file path to input hap-IBD results
+		gt_file (str): file path to input ground truth segment results
+		output_hap (str): desired output path for hap-IBD results
+		output_gt (str): desired output path for ground truth segments
+	"""
+
+	f = open(hap_ibd_res_fp)
+	f_o = open(output_hap ,'w+')
+	for line in f:
+		vals = line.split()
+		id1 = vals[0].replace("tsk_","")
+		hap1 = vals[1]
+		id2 = vals[2].replace("tsk_","")
+		hap2 = vals[3]
+		start = vals[5]
+		end = vals[6]
+		id1_f1 = int(id1) * 2 + int(hap1) -1
+		id1_f2 = int(id2) * 2 + int(hap2) -1
+		len_cm = vals[7]
+		f_o.write(str(id1_f1) + "\t" + str(id1_f2) + "\t" + start + "\t" + end + "\t" + len_cm + "\n")
+	f.close()
+	f_o.close()
 
 
 def main():
@@ -100,29 +115,25 @@ def main():
     rate_map_file = sys.argv[2]
     dataset_file = sys.argv[3]
     base_path = os.path.splitext(vcf_file)[0]
-    if checkRateMapType(rate_map_file) == "HapMap":
-        rate_map_is_plink = False
-    else:
-        rate_map_is_plink = True
-    if rate_map_is_plink:
-        rate_map_file = convert_hap_map_to_plink(rate_map_file)
+    s = time.time()
     ps_obj = p_smoother("src/P-smoother.sh")
     print("Running P-smoother...")
     ps_obj.run(f"{vcf_file}", rate_map_file)
-    if rate_map_is_plink == False:
-        rate_map_file = convert_hap_map_to_plink(rate_map_file)
     hap_ibd_obj = hap_ibd("src/hap-ibd.jar")
     hap_ibd_obj.run(f"{base_path}.smooth.vcf", rate_map_file, "hap_ibd_p_smoother_results")
-    hap_ibd_obj.run(f"{vcf_file}", rate_map_file, "hap_ibd_results")
-    if rate_map_is_plink == False:
-        subprocess.run(["rm", rate_map_file])
     subprocess.run(["gunzip", "hap_ibd_p_smoother_results.ibd.gz"])
-    subprocess.run(["gunzip", "hap_ibd_results.ibd.gz"])
-    constructDataset(vcf_file, f"{base_path}.smooth.vcf", "hap_ibd_p_smoother_results.ibd", f"hap_ibd_results.ibd", 10, rate_map_file, dataset_file)
+    print("constructing boosted dataset...")
+    formatSegmentsBoosted("hap_ibd_p_smoother_results.ibd", "hap_ibd_p_smoother_results_formatted.ibd")
+    constructBoostedDataset(vcf_file, f"{base_path}.smooth.vcf", rate_map_file, "hap_ibd_p_smoother_results_formatted.ibd", 10, dataset_file)
+    
     df = pd.read_csv(dataset_file)
     df = augmentSegments(df)
-    df_to_file(df, "ibd_boosted_segments.txt", "xgb_pred")
-    df_to_file(df, "ibd_boosted_segments_nn.txt", "nn_pred")
+    df_to_file(df, "boosted_segments_xgb.txt", "xgb_pred")
+    df_to_file(df, "boosted_segments_nn.txt", "nn_pred")
+    e = time.time()
+    time_elapsed = e - s
+    print(f"total time elapsed: {time_elapsed:.3f}")
+    subprocess.run(["rm", "hap_ibd_p_smoother_results_formatted.ibd", f"{base_path}.smooth.vcf", "hap_ibd_p_smoother_results.hbd.gz", "hap_ibd_p_smoother_results.log"])
 
 
 
